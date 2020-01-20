98c5e229e1148cdb0a874ac9dc0e97473aaf9e92
Isabel <chabelipizarro1@gmail.com>
2020-01-20 20:02:47 +0100

    add docs for production

---
+++ docs/precache-manifest.a79e76756aca7dcf7b849816276b00f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0f5d94fcea767fdfce4",
+    "url": "./welcome/static/css/main.daa54989.chunk.css"
+  },
+  {
+    "revision": "c0f5d94fcea767fdfce4",
+    "url": "./welcome/static/js/main.c0f5d94f.chunk.js"
+  },
+  {
+    "revision": "f0c32f0d7751290ab981",
+    "url": "./welcome/static/css/1.03722d7b.chunk.css"
+  },
+  {
+    "revision": "f0c32f0d7751290ab981",
+    "url": "./welcome/static/js/1.f0c32f0d.chunk.js"
+  },
+  {
+    "revision": "04bfb28e3cb356e42aed",
+    "url": "./welcome/static/js/runtime~main.04bfb28e.js"
+  },
+  {
+    "revision": "236c26683cf6b89af451bc476053ae17",
+    "url": "./welcome/index.html"
+  }
+];
